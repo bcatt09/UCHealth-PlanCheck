8546c09e258a7021e0d12da6fd40d05f9b91ef9b
Ben Catt <bcatt09@gmail.com>
2024-07-15 12:12:00 -0600

    Fixed the logic with the multiple dose level rx check

--- PlanCheck/Log.cs
+++ PlanCheck/Log.cs
@@ -10,55 +10,55 @@ using VMS.TPS.Common.Model.API;
 
 namespace PlanCheck
 {
-    public static class Log
-    {
-        private static string LogName = "ESAPIScripts";
-        private static string DefaultLogFileName = LogName + ".log";
+    //public static class Log
+    //{
+    //    private static string LogName = "ESAPIScripts";
+    //    private static string DefaultLogFileName = LogName + ".log";
 
-        public static void Initialize(ScriptContext context)
-        {
-            var config = new NLog.Config.LoggingConfiguration();
+    //    public static void Initialize(ScriptContext context)
+    //    {
+    //        var config = new NLog.Config.LoggingConfiguration();
 
-            // Targets where to log to: File and Console
-            var logfile = new NLog.Targets.FileTarget("logfile")
-            {
-                FileName = GetDefaultLogPath(),
-                Layout = "${date:format=HH\\:mm\\:ss:padding=-10:fixedlength=true} ${gdc:item=Script:padding=-20:fixedlength=true} ${level:uppercase=true:padding=-10:fixedlength=true} ${gdc:item=User:padding=-35:fixedlength=true} ${gdc:item=Patient:padding=-35:fixedlength=true} ${gdc:item=Plan:padding=-35:fixedlength=true} ${message}${onexception:${newline}  ${exception:format=Message,StackTrace:separator=\r\n}}"
-            };
+    //        // Targets where to log to: File and Console
+    //        var logfile = new NLog.Targets.FileTarget("logfile")
+    //        {
+    //            FileName = GetDefaultLogPath(),
+    //            Layout = "${date:format=HH\\:mm\\:ss:padding=-10:fixedlength=true} ${gdc:item=Script:padding=-20:fixedlength=true} ${level:uppercase=true:padding=-10:fixedlength=true} ${gdc:item=User:padding=-35:fixedlength=true} ${gdc:item=Patient:padding=-35:fixedlength=true} ${gdc:item=Plan:padding=-35:fixedlength=true} ${message}${onexception:${newline}  ${exception:format=Message,StackTrace:separator=\r\n}}"
+    //        };
 
-            // Rules for mapping loggers to targets            
-            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile);
+    //        // Rules for mapping loggers to targets            
+    //        config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile);
 
-            // Apply config           
-            LogManager.Configuration = config;
+    //        // Apply config           
+    //        LogManager.Configuration = config;
 
-            GlobalDiagnosticsContext.Set("Script", "KCI Plan Check");
-            GlobalDiagnosticsContext.Set("User", $"{context.CurrentUser.Name} ({context.CurrentUser.Id})");
-            GlobalDiagnosticsContext.Set("Patient", $"{context.Patient.LastName}, {context.Patient.FirstName} ({context.Patient.Id})");
-            GlobalDiagnosticsContext.Set("Plan", $"{context.PlanSetup.Id} ({context.Course.Id})");
+    //        GlobalDiagnosticsContext.Set("Script", "KCI Plan Check");
+    //        GlobalDiagnosticsContext.Set("User", $"{context.CurrentUser.Name} ({context.CurrentUser.Id})");
+    //        GlobalDiagnosticsContext.Set("Patient", $"{context.Patient.LastName}, {context.Patient.FirstName} ({context.Patient.Id})");
+    //        GlobalDiagnosticsContext.Set("Plan", $"{context.PlanSetup.Id} ({context.Course.Id})");
 
-            // Clear the log every day and save yesterday's log in case there were errors that need to be looked into
-            if (File.Exists(GetDefaultLogPath()) && DateTime.Now.Month != File.GetLastWriteTime(GetDefaultLogPath()).Month)
-            {
-                File.Delete(GetOldLogPath());
-                File.Copy(GetDefaultLogPath(), GetOldLogPath());
-                File.Delete(GetDefaultLogPath());
-            }
-        }
+    //        // Clear the log every day and save yesterday's log in case there were errors that need to be looked into
+    //        if (File.Exists(GetDefaultLogPath()) && DateTime.Now.Month != File.GetLastWriteTime(GetDefaultLogPath()).Month)
+    //        {
+    //            File.Delete(GetOldLogPath());
+    //            File.Copy(GetDefaultLogPath(), GetOldLogPath());
+    //            File.Delete(GetDefaultLogPath());
+    //        }
+    //    }
 
-        private static string GetDefaultLogPath()
-        {
-            return Path.Combine(GetAssemblyDirectory(), DefaultLogFileName);
-        }
+    //    private static string GetDefaultLogPath()
+    //    {
+    //        return Path.Combine(GetAssemblyDirectory(), DefaultLogFileName);
+    //    }
 
-        private static string GetOldLogPath()
-        {
-            return Path.Combine(GetAssemblyDirectory(), LogName + ".old.log");
-        }
+    //    private static string GetOldLogPath()
+    //    {
+    //        return Path.Combine(GetAssemblyDirectory(), LogName + ".old.log");
+    //    }
 
-        private static string GetAssemblyDirectory()
-        {
-            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        }
-    }
+    //    private static string GetAssemblyDirectory()
+    //    {
+    //        return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    //    }
+    //}
 }
--- PlanCheck/Script.cs
+++ PlanCheck/Script.cs
@@ -7,31 +7,65 @@ using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
 using System.Windows.Input;
 using PlanCheck;
+using PVH_Log;
+using System.Reflection;
 
 namespace VMS.TPS
 {
 	public class Script
 	{
-		public void Execute(ScriptContext context)
+        private static PVH_Logger instance;
+
+        private static string logName = "PVH_Scripts";
+        private static string logFilename = logName + ".log";
+        private static string logRelativePath = $@"PVH\{logFilename}";
+        private string logFullPath;
+
+        private static string oldLogFilename = logName + ".old.log";
+        private static string oldLogRelativePath = $@"PVH\{oldLogFilename}";
+        private string oldLogFullPath;
+
+        private string scriptName;
+        private string userName;
+        private string patientName;
+        private string planOrImageName;
+
+        public void Initialize(string scriptName, ScriptContext context, string exePath)
+        {
+            this.scriptName = scriptName;
+            userName = context.CurrentUser.Name;
+            patientName = $"{context.Patient.LastName}, {context.Patient.FirstName} ({context.Patient.Id})";
+            planOrImageName = context.PlanSetup != null ? $"{context.PlanSetup.Id} ({context.Course})" : context.Image.Id;
+
+            logFullPath = System.IO.Path.Combine(exePath, logRelativePath);
+            oldLogFullPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), oldLogRelativePath);
+
+            // Clear the log every day and save yesterday's log in case there were errors that need to be looked into
+            
+                System.IO.File.Copy(logFullPath, oldLogFullPath, true);
+                System.IO.File.Delete(logFullPath);
+        }
+        public void Execute(ScriptContext context)
 		{
-			var window = new MainWindow();
-			window.KeyDown += (object sender, KeyEventArgs e) => { if (e.Key == Key.Escape) window.Close(); };
+            //Initialize("test", context, System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            var window = new MainWindow();
+            window.KeyDown += (object sender, KeyEventArgs e) => { if (e.Key == Key.Escape) window.Close(); };
 
-			if (context.StructureSet == null)
-			{
-				MessageBox.Show("Image must have a Structure Set before running this script", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-				throw new ApplicationException("Image must have a Structure Set before running this script", new NullReferenceException());
-			}
-			window.SizeToContent = SizeToContent.WidthAndHeight;
-			window.MaxHeight = SystemParameters.WorkArea.Height * 0.95;
-			window.MaxWidth = SystemParameters.WorkArea.Width * 0.95;
-			window.Title = $"PVH - Plan Check";
+            if (context.StructureSet == null)
+            {
+                MessageBox.Show("Image must have a Structure Set before running this script", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw new ApplicationException("Image must have a Structure Set before running this script", new NullReferenceException());
+            }
+            window.SizeToContent = SizeToContent.WidthAndHeight;
+            window.MaxHeight = SystemParameters.WorkArea.Height * 0.95;
+            window.MaxWidth = SystemParameters.WorkArea.Width * 0.95;
+            window.Title = $"PVH - Plan Check";
 
-			ViewModel viewModel = new ViewModel(context);
-			window.DataContext = viewModel;
+            ViewModel viewModel = new ViewModel(context);
+            window.DataContext = viewModel;
 
-			window.ShowDialog();
-		}
+            window.ShowDialog();
+        }
 	}
 }
 
