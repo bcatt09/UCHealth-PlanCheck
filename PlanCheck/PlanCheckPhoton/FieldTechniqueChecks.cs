d72203260ab1af084867ada68ee63fd7861f6d59
Ben Catt <bcatt09@gmail.com>
2025-05-30 08:45:59 -0600

    Changed wording for APBI cases

--- PlanCheck/PlanCheckPhoton/FieldTechniqueChecks.cs
+++ PlanCheck/PlanCheckPhoton/FieldTechniqueChecks.cs
@@ -19,7 +19,7 @@ namespace PlanCheck.Checks
             DisplayName = "Field Technique";
             Result = "";
             ResultDetails = "";
-            TestExplanation = "Checks that SRS ARC / SRS STATIC is used appropriately (> 500 cGy / fx)";
+            TestExplanation = "Checks that SRS ARC / SRS STATIC is used appropriately (> 500 cGy / fx)\nAPBI breast should not use SRS ARC";
             ResultColor = ResultColorChoices.Pass;
 
             var SBRT = plan.DosePerFraction > new DoseValue(500, DoseValue.DoseUnit.cGy);
@@ -32,7 +32,7 @@ namespace PlanCheck.Checks
 
                 if (!nonSrsBeams.Any())
                     Result = "Pass";
-
+                
                 foreach (var beam in nonSrsBeams)
                 {
                     Result = "The daily dose is > 500 cGy\nShould the following fields be SRS technique for a signoff at the machine?";
@@ -49,7 +49,10 @@ namespace PlanCheck.Checks
 
                 foreach (var beam in plan.Beams.Where(x => !x.IsSetupField && x.Technique.ToString().Contains("SRS")))
                 {
-                    Result = "The following fields use an SRS technique when the daily dose is <= 500 cGy";
+                    if (breastAPBI)
+                        Result = "The following fields use an SRS technique when the treatment is a breast APBI";
+                    else
+                        Result = "The following fields use an SRS technique when the daily dose is <= 500 cGy";
                     ResultColor = ResultColorChoices.Fail;
                     ResultDetails += $"{beam.Id}\n";
                 }
